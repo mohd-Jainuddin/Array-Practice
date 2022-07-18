40516a3f315a0f744b9b988b9d290dfad028b946
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2022-07-18 17:27:55 +0530

    18-07-2022

---
+++ string problem-4/app.js
@@ -0,0 +1,6 @@
+// How to sepret a string with '-'?
+
+let str = 'jainuddin'
+ 
+let newstr = str.split('')
+console.log(newstr.join('-'))
---
+++ string problem-4/readme.md
@@ -0,0 +1,2 @@
+<h1>Output</h1>
+<img src="/img/Screenshot (157).png" alt="img">
